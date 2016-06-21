257bf235084a890d59ffe98cb896a9a474c09f3e
Matt Gibbs <mgibbs189@users.noreply.github.com>
2016-06-20 22:23:02 -0400

    Update edd-no-logins.php

--- edd-no-logins.php
+++ edd-no-logins.php
@@ -4,22 +4,7 @@ Plugin Name: EDD - No Logins
 Plugin URI: https://facetwp.com/
 Description: Allow users to access their purchase information without logging in
 Version: 1.0
-Author: Matt Gibbs
-
-Copyright 2016 Matt Gibbs
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
+Author: FacetWP, LLC
 */
 
 defined( 'ABSPATH' ) or exit;
